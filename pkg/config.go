9ff074c0b9c05d48de946965c04e7657bd574486
Michal Swietek <michael.swietek@alephzero.org>
2020-04-15 18:12:16 +0200

    implements Gen

--- pkg/config.go
+++ pkg/config.go
@@ -1,7 +1,12 @@
 package pkg
 
-import "math/big"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 var (
-	P, _ = big.NewInt(0).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+	P, _             = big.NewInt(0).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+	q, _             = big.NewInt(0).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+	reader io.Reader = rand.Reader
 )
--- pkg/distributed_secret.go
+++ pkg/distributed_secret.go
@@ -1,8 +1,13 @@
 package pkg
 
 import (
-	"gitlab.com/alephledger/core-go/pkg/network"
+	"crypto/rand"
+	"fmt"
 	"math/big"
+	"time"
+
+	"./crypto/commitment"
+	"./sync"
 )
 
 // DSecret is a distributed secret
@@ -25,6 +30,59 @@ type TDSecret interface {
 }
 
 // Gen generates a new distributed key with given label
-func Gen(label string, network network.Server, commType string) ADSecret {
+func Gen(label string, server sync.Server, egf commitment.ElGamalFactory, start time.Time) (ADSecret, error) {
+	ads := &adsecret{label: label, server: server}
+	ads.secret = rand.Int(randReader, q)
+	ads.r = rand.Int(randReader, q)
+
+	ads.eg = egf.Create(a, r)
+	zkp := []byte("R_EGKnow")
+	toSend := append(zkp, ads.eg.Marshal()...)
+
+	// TODO: reimplement after ZKPs are implemented
+	check := func(data []byte) error {
+		if len(data) < len(zkp)+2 {
+			return fmt.Errorf("received too short data %d", len(data))
+		}
+
+		return data == zkp
+	}
+
+	data, _, err := ads.server.Round(toSend, check, start)
+	if err != nil {
+		return nil, err
+	}
+
+	ads.egs = make([]commitment.ElGamal, len(data))
+	for i := range data {
+		egMarsh := data[i][len(zkp):]
+		ads.egs[i] = commitment.ElGamal{}.Unmarshal(egMarsh)
+	}
+
 	return nil
+}
+
+type dsecret struct {
+	label  string
+	secret *big.Int
+	server sync.Server
+}
+
+func (ds *dsecret) Label() string {
+	return ds.label
+}
+
+func (ds *dsecret) Reveal() (*big.Int, error) {
+	return nil, nil
+}
+
+func (ds *dsecret) Exp() (DKey, error) {
+	return nil, nil
+}
+
+type adsecret struct {
+	dsecret
+	r   *big.Int
+	eg  commitment.ElGamal
+	egs []commitment.ElGamal
 }
