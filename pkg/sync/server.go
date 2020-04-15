cd1910d73b812762bc770a414cf601eda553e630
Michal Swietek <michael.swietek@alephzero.org>
2020-04-15 17:06:32 +0200

    refactor

--- pkg/sync/server.go
+++ pkg/sync/server.go
@@ -1,5 +1,5 @@
-// Package network in it current form works only for one round going on at a time
-package network
+// Package sync in it current form works only for one round going on at a time
+package sync
 
 import (
 	"fmt"
@@ -8,73 +8,74 @@ import (
 	"time"
 
 	"gitlab.com/alephledger/core-go/pkg/network"
+	"gitlab.com/alephledger/core-go/pkg/network/tcp"
 )
 
 const (
 	timeout = time.Second
 )
 
-/*
-GER
-    sample a_k,
-    NMC(g^{a_k}, pi_k)
-    round
-    decommit
-    round
-
-GEN
-    sample a_k, r_k
-    ElGamal(a_k, r_k), R
-    round
-*/
-
-// Network represents
-type Network interface {
-	Round([]byte, func([]byte) error, time.Time)
+// Server implements
+type Server interface {
+	Round([]byte, func([]byte) error, time.Time) ([][]byte, []uint16, error)
 }
 
-type network struct {
+type server struct {
+	sync.Mutex
 	pid, nProc uint16
-	requests   []chan []byte
 	roundTime  time.Duration
 	net        network.Server
 }
 
+// New construcs a SyncServer object
+func New(pid, nProc uint16, roundTime time.Duration, localAddr string, remoteAddrs []string) Server {
+	net, _ := tcp.NewServer(localAddr, remoteAddrs)
+	return &server{
+		pid:       pid,
+		nProc:     nProc,
+		roundTime: roundTime,
+		net:       net,
+	}
+}
+
 // TODO: return only important data in [][]byte without bytes corresponding to proofs
-func (n *network) Round(toSend []byte, check func(data []byte) error, start time.Time) ([][]byte, []uint16, error) {
+func (s *server) Round(toSend []byte, check func(data []byte) error, start time.Time) ([][]byte, []uint16, error) {
+	s.Lock()
+	defer s.Unlock()
+
 	// TODO: temporary solution for scheduling the start, rewrite this ugly sleep
 	d := start.Sub(time.Now())
 	if d < 0 {
 		return nil, nil, fmt.Errorf("the start time has passed %v ago", -d)
 	}
-	time.sleep(d)
+	time.Sleep(d)
 
-	err := sendToAll(toSend)
+	err := s.sendToAll(toSend)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	roundDeadline := start.Add(n.roundTime)
-	data, missing, err := receiveFromAll(roundDeadline)
+	roundDeadline := start.Add(s.roundTime)
+	data, missing, err := s.receiveFromAll(roundDeadline)
 	if err != nil {
 		return nil, missing, err
 	}
 
 	errors := []error{}
 	wrong := []uint16{}
-	for pid := uint16(0); pid < n.nProc; pid++ {
-		if pid == n.Pid {
+	for pid := uint16(0); pid < s.nProc; pid++ {
+		if pid == s.pid {
 			continue
 		}
 		err = check(data[pid])
 		if err != nil {
-			errors = append(error, err)
+			errors = append(errors, err)
 			wrong = append(wrong, pid)
 		}
 	}
 
 	if len(wrong) > 0 {
-		return nil, wrong, fmt.Fprint("Data sent by the parties %v is wrong with errors %v", wrong, errors)
+		return nil, wrong, fmt.Errorf("Data sent by the parties %v is wrong with errors %v", wrong, errors)
 	}
 
 	// TODO: better timeout handling
@@ -83,20 +84,20 @@ func (n *network) Round(toSend []byte, check func(data []byte) error, start time
 		return nil, nil, fmt.Errorf("receiving took to long %v", -d)
 	}
 
-	return data, nil
+	return data, nil, nil
 }
 
-func (n *network) sendToAll(toSend []byte) error {
+func (s *server) sendToAll(toSend []byte) error {
 	wg := sync.WaitGroup{}
-	wg.Add(nProc - 1)
-	errors := make([]error, n.nProc)
-	for pid := uint16(0); pid < n.nProc; pid++ {
-		if pid == n.pid {
+	wg.Add(int(s.nProc) - 1)
+	errors := make([]error, s.nProc)
+	for pid := uint16(0); pid < s.nProc; pid++ {
+		if pid == s.pid {
 			continue
 		}
 		go func(pid uint16) {
 			defer wg.Done()
-			conn, err := n.net.Dial(pid, timeout)
+			conn, err := s.net.Dial(pid, timeout)
 			if err != nil {
 				errors[pid] = err
 				return
@@ -107,12 +108,12 @@ func (n *network) sendToAll(toSend []byte) error {
 				errors[pid] = err
 				return
 			}
-			_, err = conn.Flush()
+			err = conn.Flush()
 			if err != nil {
 				errors[pid] = err
 				return
 			}
-			_, err = conn.Close()
+			err = conn.Close()
 			if err != nil {
 				errors[pid] = err
 				return
@@ -123,12 +124,12 @@ func (n *network) sendToAll(toSend []byte) error {
 	wg.Wait()
 
 	var b strings.Builder
-	for pid := uint16(0); pid < n.nProc; pid++ {
-		if pid == n.pid {
+	for pid := uint16(0); pid < s.nProc; pid++ {
+		if pid == s.pid {
 			continue
 		}
 		if errors[pid] != nil {
-			fmt.Fprintf(b, "pid: %d, error: %v\n", pid, errors[pid])
+			fmt.Fprintf(&b, "pid: %d, error: %v\n", pid, errors[pid])
 		}
 	}
 
@@ -139,17 +140,17 @@ func (n *network) sendToAll(toSend []byte) error {
 	return nil
 }
 
-func (n *network) receiveFromAll(roundDeadline) ([][]byte, []uint16, error) {
-	data := make([][]byte, n.nProc)
+func (s *server) receiveFromAll(roundDeadline time.Time) ([][]byte, []uint16, error) {
+	data := make([][]byte, s.nProc)
 	missing := []uint16{}
 
 	wg := sync.WaitGroup{}
-	wg.Add(nProc - 1)
-	errors := make([]error, n.nProc)
-	for pid := uint16(0); pid < n.nProc; pid++ {
+	wg.Add(int(s.nProc) - 1)
+	errors := make([]error, s.nProc)
+	for pid := uint16(0); pid < s.nProc; pid++ {
 		go func(pid uint16) {
 			defer wg.Done()
-			conn, err := n.net.Listen(timeout)
+			conn, err := s.net.Listen(timeout)
 			if err != nil {
 				errors[pid] = err
 				return
@@ -160,7 +161,7 @@ func (n *network) receiveFromAll(roundDeadline) ([][]byte, []uint16, error) {
 				errors[pid] = err
 				return
 			}
-			_, err = conn.Close()
+			err = conn.Close()
 			if err != nil {
 				errors[pid] = err
 				return
@@ -177,12 +178,12 @@ func (n *network) receiveFromAll(roundDeadline) ([][]byte, []uint16, error) {
 	}
 
 	var b strings.Builder
-	for pid := uint16(0); pid < n.nProc; pid++ {
-		if pid == n.pid {
+	for pid := uint16(0); pid < s.nProc; pid++ {
+		if pid == s.pid {
 			continue
 		}
 		if errors[pid] != nil {
-			fmt.Fprintf(b, "pid: %d, error: %v\n", pid, errors[pid])
+			fmt.Fprintf(&b, "pid: %d, error: %v\n", pid, errors[pid])
 			// TODO: not all erros should be treated as missing
 			missing = append(missing, pid)
 		}
