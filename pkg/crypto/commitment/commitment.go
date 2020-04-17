7d3ac3fccfee08682328bc49f177da3fd208c108
Jedrzej Kula <jedrzej.kula@alephzero.org>
2020-04-17 04:48:10 +0200

    Fix comments style

--- pkg/crypto/commitment/commitment.go
+++ pkg/crypto/commitment/commitment.go
@@ -14,7 +14,7 @@ func NewElGamalFactory(h group.Elem) *ElGamalFactory {
 	return egf
 }
 
-//ElGamalFactory Factory for ElGamal-type Commitment
+//ElGamalFactory is a factory for ElGamal-type Commitment
 type ElGamalFactory struct {
 	h       group.Elem
 	neutral *ElGamal
@@ -25,12 +25,12 @@ func NewElGamal(a, b *big.Int) *ElGamal {
 	return &ElGamal{group.NewElem(a), group.NewElem(b)}
 }
 
-//ElGamal Implementation of ElGamal-type Commitments
+//ElGamal is an implementation of ElGamal-type Commitments
 type ElGamal struct {
 	first, second group.Elem
 }
 
-//Create Create new ElGamal Commitment
+//Create creates new ElGamal Commitment
 func (e *ElGamalFactory) Create(value, r *big.Int) *ElGamal {
 	return &ElGamal{
 		first:  group.NewElem(big.NewInt(0)).Mult(group.Gen, r),
@@ -38,7 +38,7 @@ func (e *ElGamalFactory) Create(value, r *big.Int) *ElGamal {
 	}
 }
 
-//Neutral Create neutral element for compose operation of ElGamal Commitments
+//Neutral creates neutral element for compose operation of ElGamal Commitments
 func (e *ElGamalFactory) Neutral() *ElGamal {
 	return e.Create(big.NewInt(0), big.NewInt(0))
 }
@@ -48,33 +48,33 @@ func (e *ElGamalFactory) IsNeutral(a *ElGamal) bool {
 	return a.Cmp(e.neutral, a)
 }
 
-//Compose Compose two ElGamal Commitments
+//Compose composes two ElGamal Commitments
 func (c *ElGamal) Compose(a, b *ElGamal) *ElGamal {
 	c.first.Add(a.first, b.first)
 	c.second.Add(a.second, b.second)
 	return c
 }
 
-//Exp Perform exp operation on ElGamal Commitments
+//Exp performs exp operation on ElGamal Commitments
 func (c *ElGamal) Exp(x *ElGamal, n *big.Int) *ElGamal {
 	c.first.Mult(x.first, n)
 	c.second.Mult(x.second, n)
 	return c
 }
 
-//Inverse Return inversed element for given ElGamal Commitment
+//Inverse returns inversed element for given ElGamal Commitment
 func (c *ElGamal) Inverse(a *ElGamal) *ElGamal {
 	c.first.Inverse(a.first)
 	c.second.Inverse(a.second)
 	return c
 }
 
-//Cmp Compare to ElGamal ElGamals (maybe should be called equal)
+//Cmp compares to ElGamal ElGamals (maybe should be called equal)
 func (*ElGamal) Cmp(a, b *ElGamal) bool {
 	return (a.first).Cmp(a.first, b.first) && (a.second).Cmp(a.second, b.second)
 }
 
-//Marshal Marshal ElGamal Commitment
+//Marshal marshals ElGamal Commitment
 func (c *ElGamal) Marshal() []byte {
 	firstBytes := c.first.Marshal()
 	secondBytes := c.second.Marshal()
@@ -87,7 +87,7 @@ func (c *ElGamal) Marshal() []byte {
 	return result
 }
 
-//Unmarshal Unmarshal ElGamal Commitment
+//Unmarshal unmarshals ElGamal Commitment
 func (c *ElGamal) Unmarshal(b []byte) *ElGamal {
 	firstLen := binary.LittleEndian.Uint32(b[0:4])
 	c.first.Unmarshal(b[4 : 4+firstLen])
--- pkg/crypto/group/group.go
+++ pkg/crypto/group/group.go
@@ -22,46 +22,46 @@ type groupElem struct {
 	value *big.Int
 }
 
-//NewElem Create new instance of Elem based on given value
+//NewElem creates new instance of Elem based on given value
 func NewElem(value *big.Int) Elem {
 	return groupElem{value}
 }
 
-//Operation Perform based operation between two Elems
+//Add performs based operation between two Elems
 func (g groupElem) Add(a, b Elem) Elem {
 	g.value.Add(a.(groupElem).value, b.(groupElem).value)
 	return g
 }
 
-//Exp Perform exp operation on given Elem and value
+//Mult performs exp operation on given Elem and value
 func (g groupElem) Mult(h Elem, x *big.Int) Elem {
 	g.value.Mul(h.(groupElem).value, x)
 	return g
 }
 
-//Inverse Return inversed element for given Elem
+//Inverse returns inversed element for given Elem
 func (g groupElem) Inverse(h Elem) Elem {
 	g.value.Mul(h.(groupElem).value, big.NewInt(-1))
 	return g
 }
 
-//Cmp Compare two groupElems (equal?)
+//Cmp compares two groupElems (equal?)
 func (groupElem) Cmp(a, b Elem) bool {
 	return a.(groupElem).value.Cmp(b.(groupElem).value) == 0
 }
 
-//Neutral Set value of this Elem to neutral value of group
+//Neutral sets value of this Elem to neutral value of group
 func (g groupElem) Neutral() Elem {
 	g.value.SetInt64(0)
 	return g
 }
 
-//Marshal Marshal Elem
+//Marshal marshals Elem
 func (g groupElem) Marshal() []byte {
 	return g.value.Bytes()
 }
 
-//Marshal Unmarshal Elem
+//Marshal unmarshals Elem
 func (g groupElem) Unmarshal(b []byte) Elem {
 	g.value.SetBytes(b)
 	return g
