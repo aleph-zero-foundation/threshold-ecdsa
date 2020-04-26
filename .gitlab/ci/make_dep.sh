82fe127bd15c364bdd96c939b0eb884a80646f08
Michal Swietek <michael.swietek@alephzero.org>
2020-04-26 15:50:16 +0200

    fixing ci conf: cut 3

--- .gitlab/ci/make_dep.sh
+++ .gitlab/ci/make_dep.sh
@@ -6,5 +6,6 @@ go get github.com/onsi/gomega/...
 go get -u golang.org/x/lint/golint
 go get github.com/ethereum/go-ethereum/crypto/secp256k1
 go get golang.org/x/crypto/hkdf
-go get gitlab.com/alephledger/core-go
+go get gitlab.com/alephledger/core-go/pkg/network
+go get gitlab.com/alephledger/core-go/pkg/tests
 # go get -v -d -t ./...
