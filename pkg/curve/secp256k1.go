b5589a6bf0fc512b06681c60dd8ed5b86a6799c8
Michal Swietek <michael.swietek@alephzero.org>
2020-04-28 13:00:31 +0200

    fixing ScalarMult

--- pkg/curve/secp256k1.go
+++ pkg/curve/secp256k1.go
@@ -67,12 +67,18 @@ func (g sGroup) Neg(a Point) Point {
 
 //scale has to be a nonnegative integer
 func (g sGroup) ScalarMult(a Point, scale *big.Int) Point {
+	if len(scale.Bytes()) > 256 {
+		scale.Mod(scale, g.Order())
+	}
 	resultX, resultY := g.curve.ScalarMult(a.(sPoint).x, a.(sPoint).y, scale.Bytes())
 	return sPoint{resultX, resultY}
 }
 
 //scale has to be a nonnegative integer
 func (g sGroup) ScalarBaseMult(scale *big.Int) Point {
+	if len(scale.Bytes()) > 256 {
+		scale.Mod(scale, g.Order())
+	}
 	resultX, resultY := g.curve.ScalarBaseMult(scale.Bytes())
 	return sPoint{resultX, resultY}
 }
