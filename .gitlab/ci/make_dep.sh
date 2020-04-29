bdbd9fd9679b1bac5acf1c0b85564f39ecde1e05
Michal Swietek <michael.swietek@alephzero.org>
2020-04-29 08:34:44 +0200

    privmult

--- .gitlab/ci/make_dep.sh
+++ .gitlab/ci/make_dep.sh
@@ -3,9 +3,15 @@ set -e
 
 go get  github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega/...
+
 go get -u golang.org/x/lint/golint
-go get github.com/ethereum/go-ethereum/crypto/secp256k1
 go get golang.org/x/crypto/hkdf
+
+go get github.com/binance-chain/tss-lib/crypto/paillier
+go get github.com/ethereum/go-ethereum/crypto/secp256k1
+
 go get gitlab.com/alephledger/core-go/pkg/network
 go get gitlab.com/alephledger/core-go/pkg/tests
+
+# go get is broken
 # go get -v -d -t ./...
--- pkg/arith/mult.go
+++ pkg/arith/mult.go
@@ -9,6 +9,7 @@ import (
 
 	"gitlab.com/alephledger/threshold-ecdsa/pkg/crypto/commitment"
 	"gitlab.com/alephledger/threshold-ecdsa/pkg/crypto/zkpok"
+	"gitlab.com/alephledger/threshold-ecdsa/pkg/sync"
 )
 
 // Mult computes a multiplication of two arithmetic secrets
@@ -19,13 +20,19 @@ func Mult(a, b *ADSecret, cLabel string) (c *ADSecret, err error) {
 	c.server = a.server
 
 	// Step 1. Compute a product of commitments to b
+	pid := -1
 	bProd := b.egf.Neutral()
-	for _, eg := range b.egs {
-		bProd.Compose(bProd, eg)
+	for id, eg := range b.egs {
+		if b.egs == nil {
+			pid = id
+			bProd.Compose(bProd, b.eg)
+		} else {
+			bProd.Compose(bProd, eg)
+		}
 	}
 
 	// Step 2. Run priv mult and compute the share of c
-	if c.secret, err = PrivMult(a.secret, b.secret); err != nil {
+	if c.secret, err = PrivMult(a.secret, b.secret, pid, nProc, a.server); err != nil {
 		return nil, err
 	}
 
@@ -134,6 +141,107 @@ func Mult(a, b *ADSecret, cLabel string) (c *ADSecret, err error) {
 }
 
 // PrivMult computes the product ab
-func PrivMult(a, b *big.Int) (*big.Int, error) {
-	return nil, nil
+func PrivMult(a, b *big.Int, pid, nProc int, server sync.Server) (*big.Int, error) {
+	// For all parties with id < pid, we act as Alice in MtA for a and Act as Bob for b.
+	// For all parties with id > pid, we act as Bob in MtA for a and act as Alice for b.
+
+	// Step 1. First round of MtA. For id < pid wait for their Enc(a') and send Enc(b), and
+	// for id > pid send Enc(a) and wait for their Enc(b').
+
+	toSendBuf1 := make([][]byte, nProc)
+	for id := range toSendBuf1 {
+		// TODO: Paillier encrypt
+		if id < pid {
+			// Bob for b
+			toSendBuf1[id] = b.Bytes()
+		} else if id > pid {
+			// Bob for a
+			toSendBuf1[id] = a.Bytes()
+		}
+	}
+
+	encShares := make([]*big.Int, nProc)
+	check := func(id uint16, data []byte) error {
+		// TODO: Paillier decrypt
+		if id < uint16(pid) {
+			// Alice for a
+			encShares[id] = new(big.Int).SetBytes(data)
+		} else if id > uint16(pid) {
+			// Alice for b
+			encShares[id] = new(big.Int).SetBytes(data)
+		}
+
+		return nil
+	}
+
+	if err := server.Round(toSendBuf1, check); err != nil {
+		return nil, err
+	}
+
+	// Step 2. Second round of MtA. For id < pid send Enc(aa'-t) and wait for E(bb'-t), and
+	// for id > pid wait for Enc(aa'-t) and send Enc(bb'-t).
+	myShares := make([]*big.Int, nProc) // collection of t_A
+	toSendBuf2 := make([][]byte, nProc)
+	genEncABmt := func(id int, share *big.Int) error {
+		var err error
+		myShares[id], err = rand.Int(randReader, Q)
+		if err != nil {
+			return err
+		}
+		// TODO: homomorphic encrypt mult
+		encABmt := new(big.Int).Mul(share, encShares[id])
+		// TODO: homomorphic encrypt add
+		encABmt.Sub(encABmt, myShares[id])
+		toSendBuf2[id] = encABmt.Bytes()
+
+		return nil
+	}
+
+	for id := range toSendBuf2 {
+		// TODO: Paillier encrypt
+		if id < pid {
+			// Alice for b
+			if err := genEncABmt(id, b); err != nil {
+				return nil, err
+			}
+		} else if id > pid {
+			// Alice for a
+			if err := genEncABmt(id, a); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	abmts := make([]*big.Int, nProc) // collection of decrypted shares for Bob
+	check = func(id uint16, data []byte) error {
+		// TODO: Paillier decrypt
+		if id < uint16(pid) {
+			// Bob for b
+			abmts[id] = new(big.Int).SetBytes(data)
+		} else if id > uint16(pid) {
+			// Bob for a
+			abmts[id] = new(big.Int).SetBytes(data)
+		}
+
+		return nil
+	}
+
+	if err := server.Round(toSendBuf2, check); err != nil {
+		return nil, err
+	}
+
+	// Step 3. Compute a share of a product of a and b
+	share := new(big.Int).Mul(a, b)
+	for id, t := range myShares {
+		if id == pid {
+			continue
+		}
+		share.Add(share, t)
+		share.Add(share, abmts[id])
+	}
+	if len(share.Bytes()) > 32 {
+		share.Mod(share, Q)
+	}
+
+	return share, nil
 }
