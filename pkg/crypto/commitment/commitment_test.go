d584eec23f1ab0ea885f75f4058dea514307812e
Michal Swietek <michael.swietek@alephzero.org>
2020-04-30 19:26:27 +0200

    changes marshal to encoding in commitment

--- pkg/crypto/commitment/commitment_test.go
+++ pkg/crypto/commitment/commitment_test.go
@@ -1,6 +1,7 @@
 package commitment_test
 
 import (
+	"bytes"
 	"math/big"
 
 	. "github.com/onsi/ginkgo"
@@ -22,35 +23,36 @@ var _ = Describe("Commitment Test", func() {
 		g = commCreator.Create(big.NewInt(3), big.NewInt(5))
 		h = commCreator.Create(big.NewInt(7), big.NewInt(11))
 	})
-	It("Cmp Diff Test", func() {
-		result := commCreator.Create(new(big.Int), new(big.Int)).Cmp(g, h)
+	It("Equal Diff Test", func() {
+		result := commCreator.Create(new(big.Int), new(big.Int)).Equal(g, h)
 		Expect(result).To(BeFalse())
 	})
-	It("Cmp Same Test", func() {
+	It("Equal Same Test", func() {
 		g = commCreator.Create(big.NewInt(7), big.NewInt(11))
-		result := g.Cmp(g, h)
+		result := g.Equal(g, h)
 		Expect(result).To(BeTrue())
 	})
 	It("Compose Test", func() {
 		result := commCreator.Create(new(big.Int), new(big.Int)).Compose(g, h)
-		cmpResult := g.Cmp(result, commCreator.Create(big.NewInt(10), big.NewInt(16)))
+		cmpResult := g.Equal(result, commCreator.Create(big.NewInt(10), big.NewInt(16)))
 		Expect(cmpResult).To(BeTrue())
 	})
 	It("Exp Test", func() {
 		result := commCreator.Create(new(big.Int), new(big.Int)).Exp(g, big.NewInt(13))
-		cmpResult := g.Cmp(result, commCreator.Create(big.NewInt(39), big.NewInt(65)))
+		cmpResult := g.Equal(result, commCreator.Create(big.NewInt(39), big.NewInt(65)))
 		Expect(cmpResult).To(BeTrue())
 	})
 	It("Inverse Test", func() {
 		result := commCreator.Create(new(big.Int), new(big.Int)).Inverse(g)
 		ord, _ := new(big.Int).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141", 16)
-		cmpResult := g.Cmp(result, commCreator.Create(new(big.Int).Sub(ord, big.NewInt(3)), new(big.Int).Sub(ord, big.NewInt(5))))
+		cmpResult := g.Equal(result, commCreator.Create(new(big.Int).Sub(ord, big.NewInt(3)), new(big.Int).Sub(ord, big.NewInt(5))))
 		Expect(cmpResult).To(BeTrue())
 	})
 	It("Marshal-Unmarshal Test", func() {
-		gm, _ := g.MarshalBinary()
-		_ = h.UnmarshalBinary(gm)
-		cmpResult := g.Cmp(h, g)
-		Expect(cmpResult).To(BeTrue())
+		rw := bytes.Buffer{}
+		err := g.Encode(&rw)
+		Expect(err).NotTo(HaveOccurred())
+		err = h.Decode(&rw)
+		Expect(g.Equal(g, h)).To(BeTrue())
 	})
 })
