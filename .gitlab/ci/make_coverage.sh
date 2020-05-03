e06a357c68837f2d68c4d1739a1856c14e4f67c2
Michal Swietek <michael.swietek@alephzero.org>
2020-05-03 13:32:02 +0200

    fixes coverage

--- .gitlab/ci/make_coverage.sh
+++ .gitlab/ci/make_coverage.sh
@@ -18,7 +18,7 @@ mkdir -p ${COVERAGE_DIR};
 
 # Create a coverage file for each package
 for package in ${PKG_LIST}; do
-    go test -covermode=count -coverprofile "${COVERAGE_DIR}/${package##*/}.cov" ${package} ;
+    go test -covermode=count -coverprofile "${COVERAGE_DIR}/${package##*/}.cov" ${package}  -ginkgo.skip Multiplying ;
 done ;
 
 # Merge the coverage profile files
