6fefb2ca68c0f8dc60462a502a70fce25a1c80a7
AdamGagol <adam.gagol@alephzero.org>
2020-06-18 16:20:27 +0200

    ZKEGKnow and ZKEGRand

---
+++ pkg/crypto/zkpok/zkpok_suite_test.go
@@ -0,0 +1,12 @@
+package zkpok_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
+)
+
+func TestPkg(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "zkpok Suite")
+}
